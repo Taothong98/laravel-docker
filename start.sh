ac8a2cf573012e43dbc5da37f43ffd007d1e7e52
Taothong98 <TaothongSSS98@gmail.com>
2024-07-19 14:32:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 # !/bin/bash
-cd /test_laravel/
-php artisan serve --host=0.0.0.0 --port=443
-
-# composer create-project laravel/laravel test_laravel2
-# cd /test_laravel2/
-# php artisan serve --host=0.0.0.0 --port=443
-
-
+cd test_laravel && php artisan serve --host=0.0.0.0 --port=443
